381299f0c680cfb709555505aaed4ea5a3ebca11
Shreyansh K Mehta <shreyansh.kumar.mehta@sap.com>
2020-04-04 19:20:42 +0530

    Add bits

---
+++ src/pages/bits.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { FaCog, FaSearch, FaGithub, FaDribbble, FaPinterest, FaTwitter, FaLinkedin } from 'react-icons/fa'
+import { Link } from "gatsby"
+
+class Bits extends React.Component {
+  render () {
+    return (
+      <div className="container">
+        dsafdsaf
+      </div>
+    )
+  }
+}
+
+export default Header
