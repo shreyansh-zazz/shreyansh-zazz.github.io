bfc44c17df3ef07325c4a3c86df029f83f4acdb6
Shreyansh Zazz <shrey.binary@gmail.com>
2020-05-06 20:47:27 +0530

    refactor: clean project structure (#35)
    Co-authored-by: Shreyansh Zazz <shrey.binary@gmail.com>

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,52 +1,16 @@
 import React from "react"
 import { graphql } from "gatsby"
-import axios from "axios"
 
 import SEO from "../components/seo"
 import "../styles/index.scss"
-import Block from "../components/block"
 
 export default class About extends React.Component {
-  state = {
-    isLoading: true,
-    activities: {},
-  }
-
-  componentDidMount() {
-    this.getRefinedData()
-  }
-
-  async getRefinedData() {
-    let activities = await this.fetchActivities()
-    this.setState({
-      activities: activities.data.map((data) => {
-        if (data.type == "PushEvent" || "PullRequestEvent" || "IssuesEvent")
-          return {
-            id: data.id,
-            eventType: data.type,
-            created_at: data.created_at,
-            payload: data.payload,
-          }
-        else return false
-      }),
-    })
-  }
-
-  fetchActivities() {
-    return new Promise((resolve, reject) => {
-      let githubActivity = axios.get(
-        "https://api.github.com/users/shreyansh-zazz/events/public?page=1&per_page=10"
-      )
-      resolve(githubActivity)
-    })
-  }
-
-  render() {
+   render() {
     return (
       <div>
         <SEO title="About" pathname={this.props.location.pathname} />
 
-        {this.state.isLoading ? <h1>Coming soon...</h1> : null}
+        Coming Soon...
       </div>
     )
   }
