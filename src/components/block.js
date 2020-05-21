fc0a66dbd75a5605c22a49e874aa8832c9cef063
Shreyansh Zazz <shrey.binary@gmail.com>
2020-05-21 22:30:38 +0530

    chore: add ad sense (#76)
    Co-authored-by: Shreyansh Zazz <shrey.binary@gmail.com>

--- src/components/block.js
+++ src/components/block.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import {ReactHelmet} from "react-helmet"
 
 import "../styles/components/block.scss"
 
@@ -17,6 +18,8 @@ export default class Block extends React.Component {
     )
     if (props.index >= 0)
       this.index = <div className="sn">{props.index + "."}</div>
+
+    this.getAd = this.getAd.bind(this)
   }
 
   getTypeColorClass() {
@@ -32,52 +35,76 @@ export default class Block extends React.Component {
     }
   }
 
+  getAd() {
+    if (this.index / 4 == 0) {
+      return (
+        <div>
+          <ins
+            class="adsbygoogle"
+            style={{display: 'block'}}
+            data-ad-format="fluid"
+            data-ad-layout-key="-gw-3+1f-3d+2z"
+            data-ad-client="ca-pub-2540012146090441"
+            data-ad-slot="9686879338"
+          ></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
-      <div className="block">
-        {this.index}
-        <div className="content">
-          <Link className="title" to={this.node.slug}>
-            {this.node.title}
-          </Link>
-          <p className="desc"> {this.node.description || this.node.excerpt}</p>
-          <div className="meta">
-            <span>
-              <Link
-                className={"category " + this.getTypeColorClass()}
-                to={this.node.category}
-              >
-                {this.node.category}
-              </Link>
-              &nbsp;|&nbsp;
-              {this.node.timeToRead}min &nbsp;|&nbsp;
-              {this.node.date}
-              &nbsp;|&nbsp;
-            </span>
-            <span>
-              {this.node.tags?.map((name, i) => {
-                var linkTag = (
-                  <span>
-                    ,&nbsp;
-                    <Link className="tag" to={"tags/" + name}>
-                      {name}
-                    </Link>
-                  </span>
-                )
-                if (!i)
-                  linkTag = (
+      <>
+        <div className="block">
+          {this.index}
+          <div className="content">
+            <Link className="title" to={this.node.slug}>
+              {this.node.title}
+            </Link>
+            <p className="desc">
+              {" "}
+              {this.node.description || this.node.excerpt}
+            </p>
+            <div className="meta">
+              <span>
+                <Link
+                  className={"category " + this.getTypeColorClass()}
+                  to={this.node.category}
+                >
+                  {this.node.category}
+                </Link>
+                &nbsp;|&nbsp;
+                {this.node.timeToRead}min &nbsp;|&nbsp;
+                {this.node.date}
+                &nbsp;|&nbsp;
+              </span>
+              <span>
+                {this.node.tags?.map((name, i) => {
+                  var linkTag = (
                     <span>
+                      ,&nbsp;
                       <Link className="tag" to={"tags/" + name}>
                         {name}
                       </Link>
                     </span>
                   )
-                return linkTag
-              })}
-            </span>
+                  if (!i)
+                    linkTag = (
+                      <span>
+                        <Link className="tag" to={"tags/" + name}>
+                          {name}
+                        </Link>
+                      </span>
+                    )
+                  return linkTag
+                })}
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+        {this.getAd()}
+      </>
     )
   }
 }
